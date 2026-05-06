6c02568ac03605636e7a570adf6a0d9748765085
saoussan <saoussan@gmail.com>
2026-05-06 19:02:16 +0200

    fix padding

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -96,7 +96,7 @@ export default function Footer() {
   };
 
   return (
-    <footer className="bg-primary text-white pt- pb-10">
+    <footer className="bg-primary text-white pt-16 pb-10">
       <div className="max-w-7xl mx-auto px-6 md:px-12">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-12 mb-16">
           {/* Brand */}
